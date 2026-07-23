4def9656a9cbbbb5fd440e1a5f99a8fff88f502c
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-07-23 18:36:24 +0700

    feat: popup style

--- events/init.lua
+++ events/init.lua
@@ -3,9 +3,20 @@ hl.on("monitor.added", function(m)
 end)
 
 hl.on("window.title", function(w)
-	-- if w.title == "hyprmon" and w.initial_title == "Ghostty" then
-	-- 	hl.dispatch(hl.dsp.window.float({ window = w, action = "set" }))
-	-- 	hl.dispatch(hl.dsp.window.center({ window = w, action = "set" }))
-	--    return
-	-- end
+    -- if w.title == "hyprmon" and w.initial_title == "Ghostty" then
+    -- 	hl.dispatch(hl.dsp.window.float({ window = w, action = "set" }))
+    -- 	hl.dispatch(hl.dsp.window.center({ window = w, action = "set" }))
+    --    return
+    -- end
+end)
+
+local popups = {
+    ["Sign in - Google Accounts — Mozilla Firefox"] = true,
+    ["Extension: (Bitwarden Password Manager) - Bitwarden — Mozilla Firefox"] = true,
+}
+
+hl.on("window.title", function(w)
+    -- if popups[w.title] then
+    --     hl.dispatch(hl.dsp.window.float({ action = "set", window = w }))
+    -- end
 end)
